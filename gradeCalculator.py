b78b11b2c7ed7864f326078bb7f9e79d188f669a
Chris Pyles <cpyles017@gmail.com>
2025-04-09 23:06:44 -0500

    changed the first line to score and input under try. Added if/elif statements with marks for each grade/score. Last 2 lines added were except and print

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,8 +3,29 @@ def calculateGrade():
     print("Calculating Grade")
     # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
 
+    try:
+        score = float(input("Enter Score: "))
+
+        if score > 1.0:
+            grade = "Bad score"
+        elif score >= 0.9 and score <= 1.0:
+            grade = "A"
+        elif score >= 0.8:
+            grade = "B"
+        elif score >= 0.7:
+            grade = "C"
+        elif score > 0.6:
+            grade = "D"
+        elif score <= 0.6 and score >= 0:
+            grade = "F"
+        else:
+            grade = "Bad score"
+
+    except:
+        grade = "Bad score"
+
+    print (grade)
     # end assignment
 
 ## If you want to test locally before you try to sync
